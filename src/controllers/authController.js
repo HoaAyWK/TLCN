94f57b2d3543d3783c7ea885d58a0a9cebd09d23
HoaAyWK <vycoi1511@gmail.com>
2022-09-30 16:49:39 +0700

    Add User model, login, and register functionality

---
+++ src/controllers/authController.js
@@ -0,0 +1,45 @@
+const { hash, compare } = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const ErrorHandler = require('../utils/errorHandler');
+const User = require('../models/User');
+
+exports.register = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const userExist = await User.findOne({ email }).lean();
+
+    if (userExist) {
+        return next(new ErrorHandler('Email already in use.', 400));
+    }
+
+    const hashedPassword = await hash(password, 10);
+    const newUser = new User({ email, password: hashedPassword });
+    const user = await newUser.save();
+
+    res.status(201).json({
+        success: true,
+        user
+    });
+};
+
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password').lean();
+
+    if (!user) {
+        return next(new ErrorHandler('User not found', 404));
+    }
+
+    const isPasswordMatched = await compare(password, user.password);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('Email or password incorrect.'));
+    }
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+};
---
+++ src/models/User.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: () => this.provider !== 'email' ? false : [true, 'Email is required'],
+        unique: true,
+    },
+    provider: {
+        type: String,
+        required: true,
+        default: 'email'
+    },
+    gooleId: {
+        type: String
+    },
+    username: {
+        type: String,
+        required: true,
+        default: this.email
+    },
+    emailConfirmed: {
+        type: Boolean,
+        default: this.provider !== 'email' ? false : true
+    },
+    firstName: {
+        type: String,
+        required: false,
+        maxLength: [100, 'First name must not be logger than 100 characters.']
+    },
+    lastName: {
+        type: String,
+        required: false,
+        maxLength: [100, 'Last name must not be logger than 100 characters.']
+    },
+    password: {
+        type: String,
+        required: () => this.provider !== 'email' ? false : [true, 'Password is required.'],
+        select: false
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: false
+        },
+        url: {
+            type: String,
+            required: false
+        }
+    },
+    roles: {
+        type: [String],
+        required: true,
+        default: ['freelancer']
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
