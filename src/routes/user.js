506323d7330162b15c4776de2b5c0b225955e049
HoaAyWK <vycoi1511@gmail.com>
2022-10-01 13:23:50 +0700

    Add crud for user profile

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,11 +3,20 @@ const { Router } = require('express');
 const {
     getAllUsers,
     getUserDetails,
+    getUserProfile,
     banUser,
     deleteUser,
+    changePassword,
+    updateUserProfile,
+    deleteMyAccount,
 } = require('../controllers/userController');
 
 const router = Router();
+
+router.route('/profile').get(getUserProfile);
+router.route('/profile/update').put(updateUserProfile);
+router.route('/password/change').put(changePassword);
+router.route('/profile/delete').delete(deleteMyAccount);
 
 router.route('/admin/users').get(getAllUsers);
 router.route('/admin/users/:id').get(getUserDetails);
